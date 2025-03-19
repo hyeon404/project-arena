eea41222fe3c6663af16f7e17e768b2b1657b5db
hyeon404 <eoqlf123@gmail.com>
2025-03-20 00:03:31 +0900

    Add Http status code
    attach test api

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
+const {StatusCodes} = require('http-status-codes');
 
 const port = process.env.PORT || 8080;
 app.listen(port);
 
-app.get('/', (req, res) => {
-    res.end('Hello world')
+app.get('/health', (req, res) => {
+    res.status(StatusCodes.OK).end('OK');
+})
+
+app.get('/error', (req, res) => {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).end();
+})
+
+app.get('/not-found', (req, res) => {
+    res.status(StatusCodes.NOT_FOUND).end();
 })
